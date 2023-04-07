511ee114ebce6c952368fb6b2e7a13f6b2183619
Dominik Andraszek <mlodz31@hotmail.com>
2023-04-07 21:12:48 +0200

    Created view
    Created view folder and frontPageview

---
+++ readily-app/src/views/frontPageview.js
@@ -0,0 +1,5 @@
+import { Container, Card, CardMedia, Typography, CardContent, CardActions, Button, IconButton, Grid } from "@mui/material";
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import Head from "next/head";
+
+export default function FrontpageView(props) {}
