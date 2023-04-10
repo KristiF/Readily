f2866ef5ad3244dd3f040e21af3bf1d3b1d99a63
Robin Persson <alf.robin.persson@gmail.com>
2023-04-10 16:49:09 +0200

    Add frontpage card component

--- readily-app/src/components/FrontpageCard.js
+++ readily-app/src/components/FrontpageCard.js
@@ -1,28 +1,29 @@
-export default function FrontpageCard(props) {
-    <Card
-        sx={{  display: 'flex', flexDirection: 'column', height:"100%"}}
-    >
-        <a href={"/articles/" + article?.data().id}>
-        <CardMedia
-        component="img"
-        loading="lazy"
-        image={article.data().image ? "https://firebasestorage.googleapis.com/v0/b/readify-a88ee.appspot.com/o/images%2F" + article.data().image + "?alt=media":null}
-        alt="random"
-        />
-        <CardContent>
-        <Typography gutterBottom variant="h5" component="h2">
-            {article?.data().title}
-        </Typography>
-            
-        <Typography noWrap>
-            {article?.data().summary}
-        </Typography>
-        </CardContent>
-        </a>
-        <Container sx={{flexGrow: 2}}/>
-        <CardActions sx={{display:"flex", alignItems:"center", justifyContent:"center"}}>
+import { Card, CardMedia, Typography, CardActions, CardContent, Container } from "@mui/material"
 
-        </CardActions>
+export default function FrontpageCard(props) {
+    return(
+        <Card sx={{  display: 'flex', flexDirection: 'column', height:"100%"}}>
+            <a href={"/articles/" + props.article.id}>
+                <CardMedia
+                component="img"
+                loading="lazy"
+                image={props.article.image ? "https://firebasestorage.googleapis.com/v0/b/readify-a88ee.appspot.com/o/images%2F" + props.article.image + "?alt=media":null}
+                alt="random"
+                />
+                <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                    {props.article.title}
+                </Typography>
+                    
+                <Typography noWrap>
+                    {props.article.summary}
+                </Typography>
+                </CardContent>
+            </a>
+            <Container sx={{flexGrow: 2}}/>
+            <CardActions sx={{display:"flex", alignItems:"center", justifyContent:"center"}}>
+            </CardActions>
            
-    </Card>
+        </Card>
+    )
 }
