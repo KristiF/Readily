92a0ccb4f230b5fb97928ee95825a4f6b8c75ca8
Sam Barati <sam_barati12@hotmail.com>
2023-05-11 22:17:13 +0200

    Removed redundant information on signup

--- readily-app/src/components/SignupCard.js
+++ readily-app/src/components/SignupCard.js
@@ -1,15 +1,13 @@
 import * as React from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import { useContext } from 'react';
-import { UserDataContext } from '@/lib/hooks';
+
 
 import { Alert } from '@mui/material';
 
@@ -67,28 +65,6 @@ export default function SignupCard(props) {
             Sign up
           </Typography>
           <Box component="form" noValidate onSubmit={handleSubmitACB} sx={{ mt: 3 }}>
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete="given-name"
-                  name="firstName"
-                  required
-                  fullWidth
-                  id="firstName"
-                  label="First Name"
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="lastName"
-                  label="Last Name"
-                  name="lastName"
-                  autoComplete="family-name"
-                />
-              </Grid>
               <Grid item xs={12}>
                 <TextField
                   required
--- readily-app/src/pages/signup/index.js
+++ readily-app/src/pages/signup/index.js
@@ -1,8 +1,14 @@
 import SignupCard from "@/components/SignupCard";
+import { useContext } from 'react';
+import { UserDataContext } from '@/lib/hooks';
 
 export default function Signup(props) {
     const {signup} = useContext(UserDataContext)
+
+    function handleSignup(userData){
+        console.log(userData.get("firstName"))
+    }
     return (
-        <SignupCard onSignUp = {signup}/>
+        <SignupCard onSignup = {handleSignup}/>
     )
 }
