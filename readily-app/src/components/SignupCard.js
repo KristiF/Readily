077808faba08eb381af2e0c21d35f782ed0212b0
Sam Barati <sam_barati12@hotmail.com>
2023-05-13 13:31:13 +0200

    Update signup card

--- readily-app/src/components/SignupCard.js
+++ readily-app/src/components/SignupCard.js
@@ -1,23 +1,21 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
 
-
-import { Alert } from '@mui/material';
-
+import { Alert } from "@mui/material";
 
 export default function SignupCard(props) {
-  function handleSubmitACB(event){
+  function handleSubmitACB(event) {
     event.preventDefault();
     const userData = new FormData(event.currentTarget);
     props.onSignup(userData);
-  };
+  }
 
   function renderErrors() {
     if (props.error) {
@@ -42,64 +40,68 @@ export default function SignupCard(props) {
           error = "Password field cannot be empty";
           break;
       }
-      return <Alert severity="error">{error}</Alert>
-
+      return <Alert severity="error">{error}</Alert>;
     }
   }
 
   return (
-      <Container component="main" maxWidth="xs">
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        {renderErrors()}
+        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
         <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
+          component="form"
+          noValidate
+          onSubmit={handleSubmitACB}
+          sx={{ mt: 3 }}
         >
-          {renderErrors()}
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign up
-          </Typography>
-          <Box component="form" noValidate onSubmit={handleSubmitACB} sx={{ mt: 3 }}>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  onChange={props.setNewEmailValue}
-                  value={props.emailValue}
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  onChange={props.setNewPasswordValue}
-                  value={props.passwordValue}
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="new-password"
-                />
-              </Grid>
-            <Button
-              type="submit"
+          <Grid item xs={12}>
+            <TextField
+              required
               fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign Up
-            </Button>
-          </Box>
+              onChange={props.setNewEmailValue}
+              value={props.emailValue}
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              required
+              fullWidth
+              onChange={props.setNewPasswordValue}
+              value={props.passwordValue}
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="new-password"
+            />
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign Up
+          </Button>
         </Box>
-      </Container>
+      </Box>
+    </Container>
   );
 }
