2eaa118352cbe67139f78247e37946b8cdc2b9b9
Kristi Francis <francis@kristi.nu>
2023-05-14 17:25:58 +0200

    add category filter in fetchArticles

--- readily-app/src/lib/fetchArticles.js
+++ readily-app/src/lib/fetchArticles.js
@@ -1,31 +1,48 @@
 import { db } from "./firebaseConfig";
 
-import { doc, getDoc, collection, getDocs, query, orderBy, limit, startAfter } from "firebase/firestore";
+import { doc, getDoc, collection, getDocs, query, orderBy, limit, startAfter, where } from "firebase/firestore";
 
-export async function fetchArticles() {
-    const articlesRef = collection(db, "articles");
-    const q = query(
+export async function fetchArticles(category="All") {
+  const articlesRef = collection(db, "articles");
+  let q;
+  (category === "All") ?
+    q = query(
       articlesRef,
       orderBy("date", "desc"),
       limit(12),
+    )
+  :
+    q = query(
+      articlesRef,
+      where("category", "==", category),
+      orderBy("date", "desc"),
+      limit(12),
     );
-  
-    const articlesSnap = await getDocs(q);
-    var articles = [];
-    articlesSnap.forEach((article) =>
-      articles.push({ ...article.data(), id: article.id })
-    );
-    return Promise.all(articles);
-  }
-export async function fetchMoreArticles(start) {
+  const articlesSnap = await getDocs(q);
+  const articles = articlesSnap.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+  console.log(`Fetched ${articles.length} articles for category '${category}'`);
+  return articles;
+
+}
+
+export async function fetchMoreArticles(category, start) {
     const articlesRef = collection(db, "articles");
-    const q = query(
+    let q;
+    (category === "All") ?
+      q = query(
         articlesRef,
         orderBy("date", "desc"),
         startAfter(start.date),
         limit(12),
-    );
-
+      )
+    :
+      q = query(
+        articlesRef,
+        where("category", "==", category),
+        orderBy("date", "desc"),
+        startAfter(start.date),
+        limit(12),
+      );  
     const articlesSnap = await getDocs(q);
     var articles = [];
     articlesSnap.forEach((article) =>
