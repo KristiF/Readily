9e1c7384a65ad2c2f0d69ae914a5bf8865791c40
Dominik Andraszek <mlodz31@hotmail.com>
2023-05-14 17:24:00 +0200

    change logincard

--- readily-app/src/components/LoginCard.js
+++ readily-app/src/components/LoginCard.js
@@ -65,8 +65,7 @@ export default function LoginCard(props) {
             <Button
               type="submit"
               fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
+              sx={{mt: 3, mb: 2 }}
             >
               Sign In
             </Button>
