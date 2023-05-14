7d6def2e75701773380face79f363bfe6d17b976
Dominik Andraszek <mlodz31@hotmail.com>
2023-05-14 13:35:15 +0200

    add offset to fetcharticles

--- readily-app/src/lib/fetchArticles.js
+++ readily-app/src/lib/fetchArticles.js
@@ -1,16 +1,38 @@
 import { db } from "./firebaseConfig";
 
-import { doc, getDoc, collection, getDocs, query, orderBy, limit } from "firebase/firestore";
+import { doc, getDoc, collection, getDocs, query, orderBy, limit, startAfter } from "firebase/firestore";
 
 export async function fetchArticles() {
-    const articlesRef = collection(db, "articles")
-    const q = query(articlesRef, orderBy("date", "desc"), limit(12))
+    const articlesRef = collection(db, "articles");
+    const q = query(
+      articlesRef,
+      orderBy("date", "desc"),
+      limit(12),
+    );
+  
+    const articlesSnap = await getDocs(q);
+    var articles = [];
+    articlesSnap.forEach((article) =>
+      articles.push({ ...article.data(), id: article.id })
+    );
+    return Promise.all(articles);
+  }
+export async function fetchMoreArticles(start) {
+    const articlesRef = collection(db, "articles");
+    const q = query(
+        articlesRef,
+        orderBy("date", "desc"),
+        startAfter(start.date),
+        limit(12),
+    );
 
     const articlesSnap = await getDocs(q);
-    var articles = []
-    articlesSnap.forEach(article=>articles.push({...article.data(), id:article.id}))
+    var articles = [];
+    articlesSnap.forEach((article) =>
+        articles.push({ ...article.data(), id: article.id })
+    );
     return Promise.all(articles);
-}
+}  
 
 export async function fetchArticle(id) {
     const docRef = doc(db, "articles", id);
