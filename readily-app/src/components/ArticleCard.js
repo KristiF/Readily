75afe1e8e6be9eaf5eb66836869875813a895546
Dominik Andraszek <mlodz31@hotmail.com>
2023-05-14 13:31:11 +0200

    add return button to article

--- readily-app/src/components/ArticleCard.js
+++ readily-app/src/components/ArticleCard.js
@@ -1,17 +1,33 @@
-import { Container, Card, CardMedia, Typography, CardContent, CardActions, Button, IconButton } from "@mui/material";
+import { Container, Card, CardMedia, Typography, CardContent, CardActions, Button, IconButton, Box } from "@mui/material";
 import FavoriteIcon from "@mui/icons-material/Favorite";
+import KeyboardReturnIcon from '@mui/icons-material/KeyboardReturn';
 export default function ArticleCard(props) {
-    console.log(props)
     return (
         <Container maxWidth="md" sx={{mt: 1, display:"flex-inline", alignItems:"center", justifyContent:"center"}}>
             <Card>
-                <CardMedia
-                component="img"
-                image={
-                    "https://firebasestorage.googleapis.com/v0/b/readify-a88ee.appspot.com/o/images%2F" 
-                    + props.props.image 
-                    + "?alt=media"}
-                />
+                <Box sx={{ position: 'relative' }}>
+                    <CardMedia
+                    component="img"
+                    image={
+                        "https://firebasestorage.googleapis.com/v0/b/readify-a88ee.appspot.com/o/images%2F" 
+                        + props.props.image 
+                        + "?alt=media"}
+                    />
+                    <Box
+                    sx={{
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    bgcolor: 'rgba(0, 0, 0, 0.1)',
+                    color: 'white',
+                    padding: '10px',
+                    }}
+                    >
+                        <IconButton onClick={()=>props.onReturn()}>
+                            <KeyboardReturnIcon/>
+                        </IconButton>
+                    </Box>
+                </Box>
                 <CardContent>
                     <Typography variant="h4">{props.props.title}</Typography>
                     <Typography sx={{fontWeight: 600}} >{}</Typography>
