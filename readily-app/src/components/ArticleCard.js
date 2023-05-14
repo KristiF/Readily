ebcd8fd6630a21488c109fea4bfd362c4afc49ef
Robin Persson <alf.robin.persson@gmail.com>
2023-05-14 19:07:32 +0200

    small fix

--- readily-app/src/components/ArticleCard.js
+++ readily-app/src/components/ArticleCard.js
@@ -24,7 +24,7 @@ export default function ArticleCard(props) {
                     }}
                     >
                         <IconButton onClick={()=>props.onReturn()}>
-                            <KeyboardReturnIcon/>
+                            <KeyboardReturnIcon sx={{color:"white"}}/>
                         </IconButton>
                     </Box>
                 </Box>
