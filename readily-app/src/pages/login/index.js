fde16fc92d5309e135ae21b04e425232aa7604ec
Kristi Francis <francis@kristi.nu>
2023-05-14 17:26:22 +0200

    update login

--- readily-app/src/pages/login/index.js
+++ readily-app/src/pages/login/index.js
@@ -1,30 +1,36 @@
 import LoginCard from "../../components/LoginCard";
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import Loading from "../../components/Loading";
 import { useRouter } from "next/router";
 import { UserDataContext } from "@/lib/hooks";
-export default function LoginPresenter(props){
+
+export default function Login(props){
     const [error, setError] = useState(false) 
     const router = useRouter()
    
-    const {logIn, loading} = useContext(UserDataContext)
+    const {user, logIn, loading} = useContext(UserDataContext)
 
     function loginUser(userData) {
         logIn(userData.get("email"), userData.get("password"))
-        .then(router.push("/"))
-        .catch(err=>setError(err.message))
-       
-        // await signInWithEmailAndPassword(auth, userData.get("email"), userData.get("password"))
-        // .then((userCredentials) => setUser(userCredentials))
-        // .catch((err)=>{setError(err);console.log(err)})
+          .catch(err => setError(err.message))
     }
+
+    useEffect(() => {
+        if (user && router.pathname === "/login" || router.pathname === "signup") {
+            router.push("/");
+        }
+        
+        if (!user) {
+            if (router.pathname !== "/login" || router.pathname !== "/signup" || router.pathname !== "/")
+                router.push("/login");  
+        }
+    }, [router.pathname, user]);
 
     return(
         <div>
             {loading ?
             <Loading/> :
-            <LoginCard onLogin={loginUser}/>
+            <LoginCard error={error} onLogin={loginUser}/>
             }
         </div>)
-
 }
